6026eea54b5696de08de5a96ef5107db7bcde738
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-12-07 10:41:13 +0100

    Create php1.php

---
+++ php1.php
@@ -0,0 +1,27 @@
+<?php
+
+$url = "http://kodi-wz-2.fritz.box:8080/jsonrpc";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data = '{"jsonrpc":"2.0","id":"1","method":"Player.Open","params":{"item":{"file":"/var/media/Toshiba4T/TVSendungen/2017-05-20 - Lola rennt.mp4"}}}';
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+
+?>
