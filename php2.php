ebfb27f97b922995d805870068ee2edc46236ef3
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-12-07 10:45:25 +0100

    Create php2.php

---
+++ php2.php
@@ -0,0 +1,10 @@
+<?php
+
+$IP = 'kodi-wz-2.fritz.box';
+require($_SERVER['DOCUMENT_ROOT']."/phpKodi-api.php");
+$_Kodi = new Kodi($IP);
+if (isset($_Kodi->error)) die($_Kodi->error);
+
+$_Kodi->openFile("/var/media/Toshiba4T/TVSendungen/2017-05-20 - Lola rennt.mp4");
+
+?>
