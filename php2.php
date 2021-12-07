07cc5caa2f217d5f5f579f3612c3e46d6b42c55d
der-optimist <45357614+der-optimist@users.noreply.github.com>
2021-12-07 11:10:46 +0100

    fix

--- php2.php
+++ php2.php
@@ -1,10 +1,12 @@
 <?php
 
-$IP = 'kodi-wz-2.fritz.box';
-require($_SERVER['DOCUMENT_ROOT']."/phpKodi-api.php");
-$_Kodi = new Kodi($IP);
-if (isset($_Kodi->error)) die($_Kodi->error);
+function php_func(){
+    $IP = 'kodi-wz-2.fritz.box';
+    require($_SERVER['DOCUMENT_ROOT']."/phpKodi-api.php");
+    $_Kodi = new Kodi($IP);
+    if (isset($_Kodi->error)) die($_Kodi->error);
 
-$_Kodi->openFile("/var/media/Toshiba4T/TVSendungen/2017-05-20 - Lola rennt.mp4");
-
+    $_Kodi->openFile("/var/media/Toshiba4T/TVSendungen/2017-05-20 - Lola rennt.mp4");
+    }
+php_func();
 ?>
